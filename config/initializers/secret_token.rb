4c7f122f19c7172303ef51478340dfe9f2acccb0
Your Name <you@example.com>
2013-05-30 12:25:15 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clipcake::Application.config.secret_token = 'e025edcb7b8fdbf7741308dacff856730cd2aed715b468aab7379a3ae1585ad2d69fddefa59a775b9d3fc1078729cd90c7cdffb838f04282811aef4a72857031'
