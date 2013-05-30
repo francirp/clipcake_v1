2f1d3e7cad9baa946ddfdfb219f937123192a6a0
Your Name <you@example.com>
2013-05-30 15:55:08 -0500

    added logic to handle user attempting to sign in via facebook once he or she has created a local account

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,25 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def self.from_omniauth(auth)
-    Facebook.where(auth.slice("provider", "uid")).first.try(:user) || create_from_omniauth(auth)
+    Facebook.where(auth.slice("provider", "uid")).first.try(:user) || where(auth.slice("email")).first.try(:add_facebook_login, auth) || create_from_omniauth(auth)
+  end
+
+  def add_facebook_login(auth)
+    if @user.present?
+      user = @user
+    else
+      user = self
+    end
+
+    f = Facebook.new
+      f.user_id = user.id
+      f.uid = auth.uid
+      f.provider = auth.provider
+      f.access_token = auth.credentials.token
+      f.access_token_expires_at = auth.credentials.expires_at
+      f.save
+
+    return user
   end
 
   def self.create_from_omniauth(auth)
@@ -18,15 +36,7 @@ class User < ActiveRecord::Base
       @user = user
     end
 
-    f = Facebook.new
-    f.user_id = @user.id
-    f.uid = auth.uid
-    f.provider = auth.provider
-    f.access_token = auth.credentials.token
-    f.access_token_expires_at = auth.credentials.expires_at
-    f.save
-
-    return @user
+    @user.add_facebook_login(auth)
   end
 
 end
